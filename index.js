2555c312d0faa5501ed5c8c30017ceecfc77c632
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-03-14 15:17:41 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // create a variable to store the reversed word
+  let reversedWord = "";
+  // loop through the word from the end to the beginning
+  for (let i = word.length - 1; i >= 0; i--) {
+    // add each letter to the reversed word
+    reversedWord += word[i];
+  }
+  // compare the reversed word to the original word
+  return reversedWord === word;
+
+  // return true if they are the same, false if they are different
+    
+
 }
 
 /* 
